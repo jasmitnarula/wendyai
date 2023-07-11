ef65400e968e99268dde06ebecda2b398851e9c3
jasmitnarula <jasmitnarula@gmail.com>
2023-07-11 15:11:34 -0600

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Chat } from './components/Chat';
+
+ const App = () => {
+
+  return (
+    <main>
+      <Chat />
+    </main>
+  );
+};
+
+export default App
---
+++ client/src/components/ChatList.jsx
@@ -0,0 +1,18 @@
+import AiChat from "./AiChat";
+import UserChat from "./UserChat";
+
+const ChatList = () => {
+  return (
+    <>
+      <UserChat />
+      <AiChat /> <UserChat />
+      <AiChat /> <AiChat /> <UserChat />
+      <AiChat /> <UserChat />
+      <AiChat /> <AiChat /> <UserChat />
+      <AiChat /> <UserChat />
+      <AiChat /> <AiChat />
+    </>
+  );
+};
+
+export default ChatList;
